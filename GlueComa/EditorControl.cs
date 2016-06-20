e56eb111cf3b8f4bd6c2eeef13dfa028389efe4a
KallDrexx <me@mshapiro.net>
2016-06-20 19:50:45 -0400

    Added basic winforms with embedded chrome

---
+++ GlueComa/EditorControl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace GlueComa
+{
+    public partial class EditorControl : UserControl
+    {
+        private ChromiumWebBrowser _browser;
+
+        public EditorControl()
+        {
+            InitializeComponent();
+        }
+
+        private void EditorControl_Load(object sender, EventArgs e)
+        {
+            Cef.Initialize(new CefSettings());
+            _browser = new ChromiumWebBrowser("www.google.com");
+            Controls.Add(_browser);
+            _browser.Dock = DockStyle.Fill;
+        }
+    }
+}
