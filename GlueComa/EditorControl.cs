0b9d439713a96752e034372c057f36d8981359b9
KallDrexx <me@mshapiro.net>
2016-06-20 21:13:25 -0400

    Added proof of concept of passing code to the javascript

--- GlueComa/EditorControl.cs
+++ GlueComa/EditorControl.cs
@@ -15,6 +15,18 @@ namespace GlueComa
             _browser = new WebBrowser();
             Controls.Add(_browser);
             _browser.Dock = DockStyle.Fill;
+
+            _browser.Navigated += _browser_Navigated;
+            _browser.DocumentCompleted += _browser_DocumentCompleted;
+        }
+
+        private void _browser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            
+        }
+
+        private void _browser_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
         }
 
         private void EditorControl_Load(object sender, EventArgs e)
@@ -27,6 +39,32 @@ namespace GlueComa
             var basePath = AppDomain.CurrentDomain.BaseDirectory;
             var htmlFolder = Path.Combine(basePath, "html");
             return Path.Combine(htmlFolder, htmlFileName);
+        }
+
+        private const string Test = @"using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VS
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ProcessStartInfo si = new ProcessStartInfo();
+			float load= 3.2e02f;
+		}
+    }
+}
+";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var param = new object[] { Test, "csharp" };
+            var result = _browser.Document.InvokeScript("loadCode", param);
         }
     }
 }
